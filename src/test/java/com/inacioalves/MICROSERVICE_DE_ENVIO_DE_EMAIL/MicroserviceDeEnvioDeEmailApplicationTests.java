10d91cdba6d69107772c01f2f876477a01168cff
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-08-02 09:39:06 -0300

    first commit

---
+++ src/test/java/com/inacioalves/MICROSERVICE_DE_ENVIO_DE_EMAIL/MicroserviceDeEnvioDeEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inacioalves.MICROSERVICE_DE_ENVIO_DE_EMAIL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDeEnvioDeEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
