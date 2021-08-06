ca2ecabe804736b545d71df5a3014b6fccddfe78
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-08-06 15:40:28 -0300

    application.propertie

---
+++ src/main/java/com/inacioalves/MICROSERVICE_DE_ENVIO_DE_EMAIL/MicroserviceDeEnvioDeEmailApplication.java
@@ -0,0 +1,13 @@
+package com.inacioalves.MICROSERVICE_DE_ENVIO_DE_EMAIL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDeEnvioDeEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDeEnvioDeEmailApplication.class, args);
+	}
+
+}
